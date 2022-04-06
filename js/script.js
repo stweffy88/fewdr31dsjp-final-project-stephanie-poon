324c16e719c3408d2ece11adfadbc76c0fc9d88b
Stephanie Poon <98735203+stweffy88@users.noreply.github.com>
2022-04-06 19:06:43 +1000

    Added JavaScript
    Open and close Off-Canvas Menu.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("testing");
+const menuController = document.getElementById('menu-hamburger');
+menuController.addEventListener('click', function() {
+  document.querySelector('.menu').classList.toggle('menu-open');
+});
+
+document.getElementById('menu-close').addEventListener('click', function() {
+  console.log('close anonymous function was clicked');
+  document.querySelector('.menu').classList.remove('menu-open');
+});
